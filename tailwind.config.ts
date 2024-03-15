44d80e4d07bb731391b0e9b5fd1cf041652081fa
huckbit <huckbit@gmail.com>
2024-03-15 00:48:19 +0000

    Update color variables in globals.scss and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        secondary: '#446f76',
+        infinitive: '#3d5371',
+        past: '#8a7852',
+        pastParticiple: '#c27658',
+      },
     },
   },
   darkMode: 'class',
