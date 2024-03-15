77cc62e1a48faf8a91325e1d830adcd4d15a1fc9
huckbit <huckbit@gmail.com>
2024-03-15 20:03:36 +0000

    Refactor NextRandomVerb component to handle loading state

--- app/ui/modules/NextRandomVerb/NextRandomVerb.tsx
+++ app/ui/modules/NextRandomVerb/NextRandomVerb.tsx
@@ -19,32 +19,38 @@ const GET_RANDOM_VERB = gql`
 export default function NextRandomVerb() {
   const [showResult, setShowResult] = useState(false);
   const { loading, error, data, refetch } = useQuery(GET_RANDOM_VERB, { client });
-
-  if (loading) return <p className='text-center'>Loading...</p>;
   if (error) return <p>An error fetching the data has occurred!</p>;
 
   const handleNext = () => {
     refetch();
-    setShowResult(false);
+    setTimeout(() => {
+      setShowResult(false);
+    }, 100);
   };
 
   return (
     <div className={style.container}>
-      <div className={style.verbContainer}>
-        <Block variant='infinitive'>{data?.randomVerb?.infinitive}</Block>
-        <Block variant='past'>{showResult ? data?.randomVerb?.past : '?'}</Block>
-        <Block variant='pastParticiple'>{showResult ? data?.randomVerb?.pastParticiple : '?'}</Block>
-      </div>
-      <div className='flex'>
-        <Button onClick={() => setShowResult(true)}>
-          Show
-          <SparklesIcon className='ml-2 h-6 w-6' />
-        </Button>
-        <Button onClick={handleNext}>
-          Next
-          <ForwardIcon className='ml-2 h-6 w-6' />
-        </Button>
-      </div>
+      {loading ? (
+        <p className='text-center'>Loading...</p>
+      ) : (
+        <>
+          <div className={style.verbContainer}>
+            <Block variant='infinitive'>{data?.randomVerb?.infinitive}</Block>
+            <Block variant='past'>{showResult ? data?.randomVerb?.past : '?'}</Block>
+            <Block variant='pastParticiple'>{showResult ? data?.randomVerb?.pastParticiple : '?'}</Block>
+          </div>
+          <div className='flex'>
+            <Button onClick={() => setShowResult(true)}>
+              Show
+              <SparklesIcon className='ml-2 h-6 w-6' />
+            </Button>
+            <Button onClick={handleNext}>
+              Next
+              <ForwardIcon className='ml-2 h-6 w-6' />
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
